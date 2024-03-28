f3d6c2898364828cc0f3b64cc15b6b87eb1e3d73
Brian Pritchett <brian.pritchett@tstllc.net>
2024-03-28 14:39:51 -0500

    Keymaps for oil
    Remove neotree

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -13,11 +13,11 @@ keymap('n', '[c', ':silent cprevious<CR>', opts)
 keymap('n', '<Leader>bb', ':Telescope buffers theme=ivy<CR>', opts)
 keymap('n', '<leader>dr', ":lua require'dap'.repl.toggle()<CR>", opts)
 keymap('n', '<leader>g', "<cmd>Neogit<cr>", opts)
-keymap('n', '<leader>e', "<cmd>Neotree toggle<cr>", opts)
+keymap('n', '<leader>e', "<cmd>new<BAR>e.<cr>", opts)
+keymap('n', '<leader>E', "<cmd>execute 'new | e ' . expand('%:h')<CR>", opts)
 keymap('n', '<C-]>', "<C-]>zz", opts)
 keymap('n', '<C-W>m', "<C-W>_<C-W>|", opts)
 
-vim.keymap.set('n', '<leader>L', function() require('logger').toggle_log() end)
 vim.keymap.set('n', ']f', '<CMD>TSTextobjectGotoNextStart @function.outer<CR>')
 vim.keymap.set('n', ']]f', '<CMD>TSTextobjectGotoNextStart @function.inner<CR>')
 vim.keymap.set('n', '[f', '<CMD>TSTextobjectGotoPreviousStart @function.outer<CR>')
@@ -28,12 +28,13 @@ vim.keymap.set('n', ']]a', '<CMD>TSTextobjectGotoNextStart @parameter.outer<CR>'
 vim.keymap.set('n', '[a', '<CMD>TSTextobjectGotoPreviousStart @parameter.inner<CR>')
 vim.keymap.set('n', '[[a', '<CMD>TSTextobjectGotoPreviousStart @parameter.outer<CR>')
 
--- vim.keymap.set('n', 'gx', function()
---   local word = vim.fn.expand('<cWORD>')
---   local command = {"/usr/bin/open", word}
---   vim.fn.system(command)
--- end)
 vim.keymap.set('t', '<C-\\>', '<C-\\><C-n>')
+
+vim.keymap.set('n', 'gx', function()
+  local word = vim.fn.expand('<cWORD>')
+  local command = { "/usr/bin/open", word }
+  vim.fn.system(command)
+end)
 
 -- Neorg
 vim.keymap.set('n', '<Leader>oc', '<CMD>Neorg capture<CR>', { silent = true })
