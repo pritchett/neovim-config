a0b4435edadcd1b68ecf64c15aa379d0c0beb4bb
Brian Pritchett <brian.pritchett@tstllc.net>
2024-12-17 14:53:02 -0600

    Update LSP settings
    Some keymaps moved to lsp (gO)
    Workaround for metals bug in scalafix file doesn't exist

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -1,14 +1,5 @@
 local M = {}
 
-vim.lsp.handlers['textDocument/codeAction'] = require 'lsputil.codeAction'.code_action_handler
--- vim.lsp.handlers['textDocument/references'] = require'lsputil.locations'.references_handler
-vim.lsp.handlers['textDocument/definition'] = require 'lsputil.locations'.definition_handler
-vim.lsp.handlers['textDocument/declaration'] = require 'lsputil.locations'.declaration_handler
-vim.lsp.handlers['textDocument/typeDefinition'] = require 'lsputil.locations'.typeDefinition_handler
-vim.lsp.handlers['textDocument/implementation'] = require 'lsputil.locations'.implementation_handler
-vim.lsp.handlers['textDocument/documentSymbol'] = vim.lsp.with(require 'lsputil.symbols'.document_handler, {
-  border = "rounded"
-})
 vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers["textDocument/signatureHelp"], {
   border = "rounded"
 })
@@ -16,29 +7,32 @@ vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
   border = "rounded",
   title = "Hover"
 })
-vim.lsp.handlers['workspace/symbol'] = require 'lsputil.symbols'.workspace_handler
 
 function M.set_keymaps(client, bufnr)
   bufnr = bufnr or 0
   local keymap = vim.keymap.set
   local opts = { noremap = true, silent = true, buffer = bufnr }
   local function with_defaults(new_opts) return vim.tbl_extend("error", opts, new_opts) end
-  keymap('n', '<leader>lcl', vim.lsp.codelens.run, with_defaults({ desc = "Run Code Lens" }))
-  -- keymap('n', 'K', vim.lsp.buf.hover, with_defaults({ desc = "Show Type Information" }))
-  keymap('n', 'gK', vim.lsp.buf.signature_help, with_defaults({ desc = "Show Signature Help" }))
-  keymap('n', 'gd', '<CMD>lua vim.lsp.buf.definition()<CR>zz', with_defaults({ desc = "Go To Definition" }))
-  keymap('n', 'gD', '<CMD>lua vim.lsp.buf.declaration()<CR>zz', with_defaults({ desc = "Go To Declaration" }))
+  keymap('i', '<C-S>', vim.lsp.buf.signature_help, with_defaults({ desc = "Show Signature Help" }))
+  -- keymap('n', 'gK', vim.lsp.buf.signature_help, with_defaults({ desc = "Show Signature Help" }))
+  keymap('n', 'grr', '<CMD>lua vim.lsp.buf.references()<CR>zz', with_defaults({ desc = "Show References" }))
+  -- keymap('n', '<leader>lca', vim.lsp.buf.code_action, with_defaults({ desc = "Run Code Action" }))
+  keymap('n', 'gra', vim.lsp.buf.code_action, with_defaults({ desc = "Run Code Action" }))
+  keymap('n', 'grn', vim.lsp.buf.rename, with_defaults({ desc = "Rename Symbol" }))
+  -- keymap('n', '<Leader>lr', vim.lsp.buf.rename, with_defaults({ desc = "Rename Symbol" }))
+  keymap('n', 'gri', vim.lsp.buf.implementation, opts)
+
   keymap('n', 'gO', '<CMD>Telescope lsp_document_symbols theme=ivy<CR>', with_defaults({ desc = "Document Symbols" }))
 
-  -- keymap(bufnr, 'n', '<C-G>i', '<CMD>lua vim.lsp.buf.implementation()<CR>zz', opts)
+  keymap('n', '<leader>lcl', vim.lsp.codelens.run, with_defaults({ desc = "Run Code Lens" }))
+  keymap('n', 'gd', '<CMD>lua vim.lsp.buf.definition()<CR>zz', with_defaults({ desc = "Go To Definition" }))
+  keymap('n', 'gD', '<CMD>lua vim.lsp.buf.declaration()<CR>zz', with_defaults({ desc = "Go To Declaration" }))
+
+  -----
   keymap('n', 'gI', '<CMD>Telescope lsp_implementations theme=ivy<CR>zz',
     with_defaults({ desc = "Go To Implementation" }))
-  keymap('n', 'gr', '<CMD>lua vim.lsp.buf.references()<CR>zz', with_defaults({ desc = "Show References" }))
-  keymap('n', '<leader>lca', vim.lsp.buf.code_action, with_defaults({ desc = "Run Code Action" }))
   keymap('n', '<Leader>ls', '<CMD>Telescope lsp_dynamic_workspace_symbols theme=ivy<CR>',
     with_defaults({ desc = "Search Workspace Symbols" }))
-  keymap('n', '<Leader>lr', vim.lsp.buf.rename, with_defaults({ desc = "Rename Symbol" }))
-  -- keymap('n', '<leader><space>', vim.lsp.buf.code_action, opts)
   keymap('n', '<leader>ld',
     function() vim.diagnostic.open_float({ border = "rounded" }, { focus = false, scope = "cursor" }) end,
     with_defaults({ desc = "Show Diagnostics" }))
@@ -46,15 +40,24 @@ function M.set_keymaps(client, bufnr)
     vim.diagnostic.setqflist({ open = false, severity = vim.diagnostic.severity.ERROR })
     vim.api.nvim_command('botright cwindow')
   end, with_defaults({ desc = "Open Diagnostics" }))
-  keymap('n', '<F9>', function() vim.diagnostic.setqflist({ severity = vim.diagnostic.severity.ERROR }) end,
-    with_defaults({ desc = "Populate quickfix with project wide compilation errors" }))
+  -- keymap('n', '<F9>', function() vim.diagnostic.setqflist({ severity = vim.diagnostic.severity.ERROR }) end,
+  -- with_defaults({ desc = "Populate quickfix with project wide compilation errors" }))
   -- keymap(bufnr, 'n', '[I', ':lua require("user.lsp").intercept("[I")<cr>', opts)
   --keymap(bufnr, 'n', '<leader>cs', ':lua vim.diagnostic.setqflist({open = false}); vim.api.nvim_command(\'botright cwindow\')<CR>', opts)
   if client.server_capabilities.selectionRangeProvider then
     keymap('n', '<leader>v', require('lsp-selection-range').trigger, with_defaults({ desc = "LSP Selection" }))
     keymap('v', '<leader>v', require('lsp-selection-range').expand, with_defaults({ desc = "Expand selection" }))
   end
+
+
+  keymap('n', '<leader>h',
+    function()
+      local inlayhints_enabled = vim.lsp.inlay_hint.is_enabled({ bufnr = 0 })
+      vim.lsp.inlay_hint.enable(not inlayhints_enabled, { bufnr = 0 })
+    end,
+    with_defaults({ desc = "Toggle inlay hints" }))
 end
+
 
 local lsp_id = vim.api.nvim_create_augroup("LSP", {})
 
@@ -88,20 +91,45 @@ function M.on_attach(client, bufnr)
     group = lsp_id
   })
 
-  require "lsp_signature".on_attach({
-    bind = true,
-    handler_opts = {
-      border = "rounded"
-    }
-  }, bufnr)
+  -- require "lsp_signature".on_attach({
+  --   bind = true,
+  --   handler_opts = {
+  --     border = "rounded"
+  --   }
+  -- }, bufnr)
 
-  vim.api.nvim_create_autocmd("BufWritePre", {
-    buffer = bufnr,
-    callback = function()
-      vim.lsp.buf.format({ async = false, id = client.id })
-    end,
-    group = lsp_id
-  })
+  vim.opt_local.statuscolumn = require('user.customizations').lsp_statuscolumn
+
+  function set_up_autoformatting()
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      callback = function()
+        if vim.g.skip_autofmt then
+          return
+        end
+        if vim.bo.filetype == "scala" or vim.bo.filetype == "sbt" then
+          vim.api.nvim_create_autocmd("BufWritePost", {
+            buffer = bufnr,
+            once = true,
+            callback = function()
+              vim.lsp.buf.format({ async = true, id = client.id })
+              vim.fn.timer_start(1000, function(_)
+                vim.cmd.update()
+              end)
+            end,
+            group = lsp_id
+          })
+        else
+          vim.lsp.buf.format({ async = false, id = client.id })
+        end
+      end,
+      group = lsp_id
+    })
+  end
+
+  if client.supports_method("textDocument/formatting") then
+    set_up_autoformatting()
+  end
 
   vim.cmd [[TSEnable highlight]]
 end
