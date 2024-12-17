cd9129bec3aa4f50d4e570b8cc1dafadf33e7e02
Brian Pritchett <brian.pritchett@tstllc.net>
2024-12-17 14:56:03 -0600

    :TelescopeResume

--- lua/user/commands.lua
+++ lua/user/commands.lua
@@ -19,6 +19,9 @@ vim.api.nvim_create_user_command("Config", function()
     }))
 end, {})
 
+vim.api.nvim_create_user_command("TelescopeResume", function()
+  require('telescope.builtin').resume({ cache_index = 2 })
+end, {})
 
 
 vim.api.nvim_create_user_command("UrlDecode", function()
