1a5d3aca14244954bd15852261aaf36c23d7f304
Brian Pritchett <brian.pritchett@tstllc.net>
2025-01-22 20:08:02 -0600

    Gitsigns keymaps

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -4,9 +4,9 @@ return {
   opts = {
     on_attach = function()
       local gs = require("gitsigns")
-      vim.keymap.set('n', '[g', function() gs.nav_hunk("prevous") end, { desc = "Previous hunk" })
+      vim.keymap.set('n', '[g', function() gs.nav_hunk("prev") end, { desc = "Previous hunk" })
       vim.keymap.set('n', '[G',
-        function() gs.nav_hunk("prevous", { preview = true }) end, { desc = "Previous hunk with preview" })
+        function() gs.nav_hunk("prev", { preview = true }) end, { desc = "Previous hunk with preview" })
       vim.keymap.set('n', ']g', function() gs.nav_hunk("next") end, { desc = "Next hunk" })
       vim.keymap.set('n', ']G',
         function() gs.nav_hunk("next", { preview = true }) end, { desc = "Next hunk with preview" })
