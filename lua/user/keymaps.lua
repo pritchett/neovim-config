1bae30464a996a0855e877ba3fc4eed3df17ee4b
Brian Pritchett <brian.pritchett@tstllc.net>
2025-01-22 20:05:56 -0600

    Luasnip keymaps
    If luasnip can be loaded

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -150,17 +150,18 @@ vim.keymap.set('n', '<a-J>', '<CMD>wincmd J<CR>')
 vim.keymap.set('n', '<a-K>', '<CMD>wincmd K<CR>')
 
 ---- luasnip
-local ls = require("luasnip")
+local ok, ls = pcall(require, "luasnip")
+if ok then
+  vim.keymap.set({ "i" }, "<C-L>", function() ls.expand() end, { silent = true })
+  vim.keymap.set({ "i", "s" }, "<C-J>", function() ls.jump(1) end, { silent = true })
+  vim.keymap.set({ "i", "s" }, "<C-K>", function() ls.jump(-1) end, { silent = true })
 
-vim.keymap.set({ "i" }, "<C-L>", function() ls.expand() end, { silent = true })
-vim.keymap.set({ "i", "s" }, "<C-J>", function() ls.jump(1) end, { silent = true })
-vim.keymap.set({ "i", "s" }, "<C-K>", function() ls.jump(-1) end, { silent = true })
-
-vim.keymap.set({ "i", "s" }, "<C-E>", function()
-  if ls.choice_active() then
-    ls.change_choice(1)
-  end
-end, { silent = true })
+  vim.keymap.set({ "i", "s" }, "<C-E>", function()
+    if ls.choice_active() then
+      ls.change_choice(1)
+    end
+  end, { silent = true })
+end
 
 
 --- terminal
