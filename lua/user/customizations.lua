8be7c8957e13719c48b4c400e0975fa0e15cdff3
Brian Pritchett <brian@Brians-Air.attlocal.net>
2025-03-27 22:28:36 -0500

    More statuscolumn

--- lua/user/customizations.lua
+++ lua/user/customizations.lua
@@ -56,10 +56,11 @@ M.get_fold_column = function()
 end
 
 M.lsp_statuscolumn = table.concat({
-  "%04{%v:lua.require('user.customizations').get_signs_minus_gitsigns()%}",
+  "%02(%{%v:lua.require('user.customizations').get_signs_minus_gitsigns()%}%)",
   "%l",
-  "%02(%=%{%v:lua.require('user.customizations').get_gitsigns_sign_column()%}%)",
-  "%{%v:lua.require('user.customizations').get_fold_column()%}"
+  "%02(%{%v:lua.require('user.customizations').get_gitsigns_sign_column()%}%)",
+  "%{%v:lua.require('user.customizations').get_fold_column()%}",
+  " "
 })
 
 return M
