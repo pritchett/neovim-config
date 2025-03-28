845626a931528499bf4e503b7624533afa545a47
Brian Pritchett <brian@brianpritchett.com>
2025-03-28 11:02:09 -0500

    Movement keymaps between headlines in norg

--- ftplugin/norg.lua
+++ ftplugin/norg.lua
@@ -1,2 +1,5 @@
 vim.keymap.set('n', 'gO', '<CMD>Neorg toc<CR>', { buffer = true })
 vim.keymap.set('n', '<Leader>ot', '<CMD>Neorg toggle-concealer<CR>', { buffer = true, desc = "Toggle concealer" })
+
+vim.keymap.set('n', ']]', function() vim.cmd([[ call search("^\\s*\\*") ]]) end)
+vim.keymap.set('n', '[[', function() vim.cmd([[ call search("^\\s*\\*", "b") ]]) end)
