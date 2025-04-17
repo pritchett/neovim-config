cbb522d7e884e53f5fd2ee2708685ecbc7bdd748
Brian Pritchett <brian@Brians-MacBook-Air.attlocal.net>
2025-04-16 19:37:33 -0500

    Remove check for telescope in scala ftplugin

--- ftplugin/scala.lua
+++ ftplugin/scala.lua
@@ -1,8 +1,1 @@
--- local dap = require('dap')
--- vim.keymap.set('n', '<F5>', dap.continue, { buffer = true })
-local ok, _ = pcall(require, 'telescope')
-if ok then
-  vim.keymap.set('n', '<leader>m', '<CMD>Telescope metals commands theme=ivy<CR>', { buffer = true })
-else
-  vim.keymap.set('n', '<leader>m', require('metals').commands, { buffer = true, desc = "Metals commands" })
-end
+vim.keymap.set('n', '<localleader>m', require('metals').commands, { buffer = true, desc = "Metals commands" })
