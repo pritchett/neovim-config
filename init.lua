6ac4eb16dc8e1474cf2d5214dd2243cdc74eedb2
Brian Pritchett <brian@Brians-Air.attlocal.net>
2025-04-18 09:02:22 -0500

    More language highlighting
    Copied from https://www.reddit.com/r/neovim/comments/1k1vh6e/lsp_for_dotfilesconfig_files_in_neovim/mnpfjfw/

--- init.lua
+++ init.lua
@@ -94,3 +94,19 @@ vim.keymap.set({ 'x', 'o' }, 'iu', function()
     results = vim.fn.matchstrpos(line, pattern, results[3])
   end
 end, { desc = "Inner UUID" })
+
+
+vim.filetype.add({
+  extension = { rasi = "rasi", rofi = "rasi", wofi = "rasi" },
+  filename = {
+    ["vifmrc"] = "vim",
+  },
+  pattern = {
+    [".*/waybar/config"] = "jsonc",
+    [".*/mako/config"] = "dosini",
+    [".*/kitty/.+%.conf"] = "kitty",
+    [".*/hypr/.+%.conf"] = "hyprlang",
+    ["%.env%.[%w_.-]+"] = "sh",
+  },
+})
+vim.treesitter.language.register("bash", "kitty")
