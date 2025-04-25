a9d76750c66a744a5438ffe4e46f882f0ab87460
Brian Pritchett <brian@brianpritchett.com>
2025-04-25 11:09:39 -0500

    Statusline fixes

--- lua/user/customizations.lua
+++ lua/user/customizations.lua
@@ -1,10 +1,12 @@
-local M = {}
+local M                    = {}
+
+local gitsigns_namespaces  = { gitsigns = true, gitsigns_blame = true, gitsigns_removed = true, gitsigns_signs_ = true, gitsigns_signs_staged = true }
 
 M.get_signs_minus_gitsigns = function()
   if vim.v.virtnum < 0 then return "" end
   local line = ""
   for k, ns in pairs(vim.api.nvim_get_namespaces()) do
-    if k ~= "gitsigns_signs_" then
+    if gitsigns_namespaces[k] == nil then
       local signs = vim.api.nvim_buf_get_extmarks(0, ns, { vim.v.lnum - 1, 0 }, { vim.v.lnum - 1, -1 }, { type = "sign" })
       for _, sign in ipairs(signs) do
         local sign_id = sign[1]
@@ -40,23 +42,23 @@ M.get_gitsigns_sign_column = function()
   return ""
 end
 
-M.get_fold_column = function()
+M.get_fold_column          = function()
   local fillchars = vim.opt.fillchars:get()
   local fold_line_before = vim.fn.foldlevel(vim.v.lnum - 1)
   local fold_line_curr = vim.fn.foldlevel(vim.v.lnum)
   local increased_fold = fold_line_curr > fold_line_before
   if increased_fold and vim.fn.foldclosed(vim.v.lnum) == vim.v.lnum then
-    return fillchars.foldclose
+    return fillchars.foldclose or ""
   elseif increased_fold then
-    return fillchars.foldopen
+    return fillchars.foldopen or ""
   elseif fold_line_curr > 0 then
-    return fillchars.foldsep
+    return fillchars.foldsep or ""
   end
-  return fillchars.fold
+  return fillchars.fold or ""
 end
 
-M.lsp_statuscolumn = table.concat({
-  "%-01(%{%v:lua.require('user.customizations').get_signs_minus_gitsigns()%}%)",
+M.lsp_statuscolumn         = table.concat({
+  "%-02(%{%v:lua.require('user.customizations').get_signs_minus_gitsigns()%}%)",
   "%l",
   "%-02(%{%v:lua.require('user.customizations').get_gitsigns_sign_column()%}%)",
   "%-02{%v:lua.require('user.customizations').get_fold_column()%}",
