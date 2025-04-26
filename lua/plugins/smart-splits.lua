36ab2e91d756cba3a1c55eb05f2fbf5a1c23a3cb
Brian Pritchett <brian@brianpritchett.com>
2025-04-26 15:53:35 -0500

    Smarts splits in terminal
    Use smart splits move/resize in terminal mode

--- lua/plugins/smart-splits.lua
+++ lua/plugins/smart-splits.lua
@@ -12,21 +12,21 @@ return {
     }
   },
   keys = {
-    { '<D-C-h>',  mode = { 'n' }, function() require('smart-splits').move_cursor_left() end },
-    { '<D-C-j>',  mode = { 'n' }, function() require('smart-splits').move_cursor_down() end },
-    { '<D-C-k>',  mode = { 'n' }, function() require('smart-splits').move_cursor_up() end },
-    { '<D-C-l>',  mode = { 'n' }, function() require('smart-splits').move_cursor_right() end },
-    { '<D-C-\\>', mode = { 'n' }, function() require('smart-splits').move_cursor_previous() end },
-    { '<D-C-b>',  mode = { 'n' }, '<CMD>wincmd b<CR>' },
-    { '<D-C-p>',  mode = { 'n' }, '<CMD>wincmd p<CR>' },
-    { '<D-C-t>',  mode = { 'n' }, '<CMD>wincmd t<CR>' },
-    { '<D-C-[>',  mode = { 'n' }, '<CMD>tabprev<CR>' },
-    { '<D-C-]>',  mode = { 'n' }, '<CMD>tabnext<CR>' },
+    { '<D-C-h>',  mode = { 'n', 't' }, function() require('smart-splits').move_cursor_left() end },
+    { '<D-C-j>',  mode = { 'n', 't' }, function() require('smart-splits').move_cursor_down() end },
+    { '<D-C-k>',  mode = { 'n', 't' }, function() require('smart-splits').move_cursor_up() end },
+    { '<D-C-l>',  mode = { 'n', 't' }, function() require('smart-splits').move_cursor_right() end },
+    { '<D-C-\\>', mode = { 'n', 't' }, function() require('smart-splits').move_cursor_previous() end },
+    { '<D-C-b>',  mode = { 'n', 't' }, '<CMD>wincmd b<CR>' },
+    { '<D-C-p>',  mode = { 'n', 't' }, '<CMD>wincmd p<CR>' },
+    { '<D-C-t>',  mode = { 'n', 't' }, '<CMD>wincmd t<CR>' },
+    { '<D-C-[>',  mode = { 'n', 't' }, '<CMD>tabprev<CR>' },
+    { '<D-C-]>',  mode = { 'n', 't' }, '<CMD>tabnext<CR>' },
 
-    { '<D-S-h>',  mode = { 'n' }, function() require('smart-splits').resize_left() end },
-    { '<D-S-j>',  mode = { 'n' }, function() require('smart-splits').resize_down() end },
-    { '<D-S-k>',  mode = { 'n' }, function() require('smart-splits').resize_up() end },
-    { '<D-S-l>',  mode = { 'n' }, function() require('smart-splits').resize_right() end },
+    { '<D-S-h>',  mode = { 'n', 't' }, function() require('smart-splits').resize_left() end },
+    { '<D-S-j>',  mode = { 'n', 't' }, function() require('smart-splits').resize_down() end },
+    { '<D-S-k>',  mode = { 'n', 't' }, function() require('smart-splits').resize_up() end },
+    { '<D-S-l>',  mode = { 'n', 't' }, function() require('smart-splits').resize_right() end },
 
     -- swapping buffers between windows
     -- vim.keymap.set('n', '<leader><leader>h', require('smart-splits').swap_buf_left)
