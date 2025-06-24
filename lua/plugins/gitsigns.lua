9c5e18dcfb0124a04b1979c33873de104dd9aa7b
Brian Pritchett <brian@brianpritchett.com>
2025-06-24 14:49:36 -0500

    Fix gitsigns command desc

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -10,6 +10,8 @@ return {
       vim.keymap.set('n', ']g', function() gs.nav_hunk("next") end, { desc = "Next hunk" })
       vim.keymap.set('n', ']G',
         function() gs.nav_hunk("next", { preview = true }) end, { desc = "Next hunk with preview" })
+      vim.keymap.set('n', '<D-g>p', function() gs.preview_hunk() end, { desc = "Preview hunk" })
+      vim.keymap.set('n', '<D-g>b', function() gs.blame_line() end, { desc = "Blame hunk" })
     end
   },
   event = "VeryLazy"
