7ab3a627b7e0beaaea9c4d5babdba6365c4a2bf8
Brian Pritchett <brian@brianpritchett.com>
2025-12-23 10:17:19 -0600

    Purescript keymaps

---
+++ after/ftplugin/purescript.lua
@@ -0,0 +1,14 @@
+vim.cmd.inoreabbrev("<buffer> forall ∀")
+vim.keymap.set('n', '<localleader>pr', '<CMD>split<CR><CMD>term npx spago run<CR>', { buffer = true })
+vim.keymap.set('n', '<localleader>ps', function()
+  vim.ui.input({ prompt = 'Pursuit search: ' }, function(input)
+    if input == '' or input == nil then return end
+    vim.cmd.split()
+    vim.cmd.term("w3m 'https://pursuit.purescript.org/search?q=" .. input .. "'")
+  end)
+end, { buffer = true })
+vim.keymap.set('n', '<localleader>pb', '<CMD>split<CR><CMD>term npx spago build<CR>', { buffer = true })
+
+vim.cmd('inoreabbrev <buffer> derivce derive')
+vim.cmd('inoreabbrev <buffer> dervice derive')
+vim.cmd('inoreabbrev <buffer> derivec derive')
