a05e8284fee543a8f70de282a09752187994b9fe
Brian Pritchett <brian@brianpritchett.com>
2025-12-23 10:17:00 -0600

    Purescript language server settings

--- lsp/purescript-language-server.lua
+++ lsp/purescript-language-server.lua
@@ -1,5 +1,11 @@
 return {
   cmd = { 'npx', 'purescript-language-server', '--stdio' },
   filetypes = { 'purescript' },
-  root_markers = { 'spago.yaml' }
+  root_markers = { 'spago.yaml' },
+  settings = {
+    purescript = {
+      formatter = "purs-tidy",
+      addSpagoSources = true
+    }
+  }
 }
