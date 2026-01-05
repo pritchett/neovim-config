35a715636c7ba4de8f5daf2ebc889fdb357e6f97
Brian Pritchett <brian@brianpritchett.com>
2026-01-05 13:11:47 -0600

    Luasnip updates

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -8,12 +8,16 @@ return {
     local t = ls.text_node
     local i = ls.insert_node
     local f = ls.function_node
+    local d = ls.dynamic_node
+    local sn = ls.snippet_node
+    local c = ls.choice_node
     local extras = require('luasnip.extras')
     local r = extras.rep
     local fmt = require('luasnip.extras.fmt').fmt
     local types = require('luasnip.util.types')
     local events = require('luasnip.util.events')
     local haskell_snippets = require('haskell-snippets').all
+
     ls.add_snippets('haskell', haskell_snippets, { key = 'haskell' })
 
     ls.add_snippets('purescript', {
@@ -37,63 +41,108 @@ return {
       )
 
     })
+    local function get_package_name(_)
+      local filename = vim.fn.expand('%:h')
+      local fn1, _ = filename:gsub("^.*src/main/scala/", "")
+      local fn2, _ = fn1:gsub("^.*src/test/scala/", "")
+      local fn3, _ = fn2:gsub("^.*src/it/scala/", "")
+      local fn4, _ = fn3:gsub("/", ".")
+      return fn4
+    end
 
     ls.add_snippets('scala', {
       s("newtype",
-        fmt(
-          [[
-          type {} = {}.Type
-          object {} extends Newtype[{}]
-          ]], {
-            i(1), r(1), r(1), i(2)
-          }
-        ),
-        {
-          callbacks = {
-            [-1] = {
-              [events.pre_expand] = function(node, event_args)
-                local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
-                local start = 0
-                local package_found = false
-                if #lines == 0 then
-                else
-                  for _, line in ipairs(lines) do
-                    if line:match("^package ") then
-                      package_found = true
-                      break
-                    end
-                  end
-                end
-                for ii, line in ipairs(lines) do
-                  if package_found and (line:match("^import") or line:len() == 0) then
-                    start = ii - 1
-                    break
-                  end
-                end
+        d(1, function(_, _, _)
+          local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+          if #lines == 1 then
+            return sn(nil,
+              fmt(
+                [[
+                {}{}
 
-                vim.api.nvim_buf_set_lines(0, start, start, false, {
-                  "import neotype.*"
+                import neotype.*
+
+                type {} = {}.Type
+                object {} extends {}[{}]
+                ]],
+                {
+                  t("package "),
+                  f(get_package_name),
+                  d(1, function()
+                    local filename = vim.fn.expand("%:t:r")
+                    return sn(nil, {
+                      i(1, filename)
+                    })
+                  end),
+                  r(1),
+                  r(1),
+                  c(2, { t("Newtype"), t("Subtype") }),
+                  i(3)
                 })
-                return nil
-              end
-            }
-          }
-        }),
+            )
+          else
+            return sn(nil, fmt(
+                [[
+              type {} = {}.Type
+              object {} extends {}[{}]
+              ]],
+                {
+                  i(1), r(1), r(1), c(2, { t("Newtype"), t("Subtype") }), i(3)
+                }
+              ),
+              {
+                node_callbacks = {
+                  [events.leave] = function(node, event_args)
+                    ---@diagnostic disable-next-line: redefined-local
+                    local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+                    local start = 0
+                    local package_found = false
+                    local found_import_neotype = false
+                    for ii, line in ipairs(lines) do
+                      if line:match("^package ") then
+                        package_found = true
+                        start = ii
+                      end
 
+                      if line:match("^import neotype%.%*") then
+                        found_import_neotype = true
+                      end
+                    end
 
-      s("package", {
-        t("package "), f(function(_)
-        local filename = vim.fn.expand('%')
-        local fn1, _ = filename:gsub("^.*src/main/scala/", "")
-        local fn2, _ = fn1:gsub("^.*src/test/scala/", "")
-        local fn3, _ = fn2:gsub("(.*)/.-%.scala$", "%1")
-        local fn4, _ = fn3:gsub("/", ".")
-        return fn4
-      end)
+                    if not found_import_neotype then
+                      for ii, line in ipairs(lines) do
+                        if package_found and (line:match("^import") or line:len() == 0) then
+                          start = ii - 1
+                          break
+                        end
+                      end
 
+                      vim.api.nvim_buf_set_lines(0, start, start, false, {
+                        "",
+                        "import neotype.*"
+                      })
+                    end
+                    return nil
+                  end
+                }
+              })
+          end
+        end)),
+      s({
+        trig = "package",
+        name = "Package",
+        desc = "Insert package name",
+        show_condition = function()
+          local pos = vim.fn.getpos('.')
+          return pos[2] == 1
+        end
+      }, {
+        t("package "),
+        f(get_package_name),
+        t({ "", "", "" })
       })
-    }
-    )
+
+    })
 
     ls.add_snippets('lua', {
       s("newproject",
