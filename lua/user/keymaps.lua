4c4c49ce6f97c86f0158e8249c98b5dd7ac1b6b2
Brian Pritchett <brian@brianpritchett.com>
2026-01-11 17:14:22 -0600

    More keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -7,29 +7,40 @@ vim.keymap.set('n', 'H', 'zH')
 vim.keymap.set('n', '<leader>p', '<CMD>Projects<CR>', { desc = "Projects" })
 
 -- keymap('n', '<leader>e', "<CMD>new<BAR>e.<cr>", opts)
-vim.keymap.set('n', '<leader>e', function()
+local edit_cwd = function()
   -- if vim.api.nvim_buf_get_name(0) == "" then
   vim.cmd.edit('.')
   -- else
   --   vim.cmd.new()
   --   vim.cmd.edit('.')
   -- end
-end, { desc = "Browse directory" })
-vim.keymap.set('n', '<leader>E', function()
+end
+vim.keymap.set('n', '<leader>e', edit_cwd, { desc = "Browse current directory" })
+vim.keymap.set('n', 'g<leader>e', function()
+  vim.cmd.split()
+  edit_cwd()
+end, { desc = "Browse current directory in new split" })
+
+local edit_buffer_dir = function()
   local dir = vim.fn.expand('%:h')
   if dir == "" then
     vim.cmd.edit('.')
   else
     vim.cmd.edit(dir)
   end
-end, { desc = "Open current of buffer" })
-vim.keymap.set('n', '<leader>E', "<CMD>execute 'e ' . expand('%:h')<CR>", { desc = "Open current of buffer" })
+end
+
+vim.keymap.set('n', '<leader>E', edit_buffer_dir, { desc = "Open current buffer directory" })
+vim.keymap.set('n', 'g<leader>E', function()
+  vim.cmd.split()
+  edit_buffer_dir()
+end, { desc = "Open current buffer directory in split" })
 
 vim.keymap.set('n', '<C-]>', "<C-]>zt", opts)
 vim.keymap.set('n', '<C-W>m', "<C-W>_<C-W>|", opts)
 
-vim.keymap.set('n', ']f', '<CMD>TSTextobjectGotoNextStart @function.outer<CR>')
-vim.keymap.set('n', '[f', '<CMD>TSTextobjectGotoPreviousStart @function.outer<CR>')
+-- vim.keymap.set('n', ']f', '<CMD>TSTextobjectGotoNextStart @function.outer<CR>')
+-- vim.keymap.set('n', '[f', '<CMD>TSTextobjectGotoPreviousStart @function.outer<CR>')
 
 -- vim.keymap.set('n', ']a', '<CMD>TSTextobjectGotoNextStart @parameter.inner<CR>')
 -- vim.keymap.set('n', '[a', '<CMD>TSTextobjectGotoPreviousStart @parameter.inner<CR>')
